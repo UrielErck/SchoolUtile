d20b2d7fcec3a358528fcca4245db5df465f9278
UrielErck <106910753+UrielErck@users.noreply.github.com>
2023-09-30 20:56:21 +0700

    Big old bug fix
    WHAT THE. WHY IT DOES NOT WORK PROPERLY, MICROSOFT?

--- icaclsEdit.py
+++ icaclsEdit.py
@@ -5,11 +5,13 @@ def create_access(data):
     path = data.get('Name')
     access = f"({','.join(data.get('Access'))})"
     remove_acces(path=path)
+    command = f'icacls "{path}"'
     if list(locale.getlocale())[0] == 'Russian_Russia':
-        print(f'icacls "{path}" /deny "Все":{access}')
-        subprocess.call(f'icacls "{path}" /deny "Все":{access}', creationflags=0x08000000)
+        subprocess.call(f'{command} /grant "Все":F', creationflags=0x08000000)
+        subprocess.call(f'{command} /deny "Все":{access}', creationflags=0x08000000)
         print('RUS')
     else:
+        subprocess.call(f'icacls "{path}" /grant "All":F', creationflags=0x08000000)
         subprocess.call(f'icacls "{path}" /deny "All":{access}', creationflags=0x08000000)
         print('ENG')
 
